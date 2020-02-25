c21fdb145567bbc6f9cf656550f5cec54d39fbdb
Thuc Phan <tcp004@ucsd.edu>
2020-02-24 17:38:58 -0800

    apis, gesture, ajax

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
 	// add your code here
+  L.mapquest.key = 'cK2Vm5FArNkXYXgIaJFkFARcnT3sAewc';
+  var map = L.mapquest.map('map', {
+    center: [32.878842, -117.235904],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  L.marker([32.878842, -117.235904]).addTo(map);
 }
